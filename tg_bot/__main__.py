4e6e451d5e7d55c65eabc9e70133428197777b6c
Skadi <63096193+Dank-del@users.noreply.github.com>
2020-06-08 17:00:22 +0530

    Update __main__.py
    Hm

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -43,7 +43,6 @@ I'm a modular group management bot with a few fun extras!
  - /settings:
    - in PM: will send you your settings for all supported modules.
    - in a group: will redirect you to pm, with all that chat's settings.
-
 {}
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
