3c5314f431af24b81da882e34fd949097aeaf9bf
Skadi <63096193+Dank-del@users.noreply.github.com>
2020-07-28 23:35:38 +0530

    Update __main__.py

--- TohsakaRobot/__main__.py
+++ TohsakaRobot/__main__.py
@@ -572,7 +572,7 @@ def main():
 
     else:
         LOGGER.info("Using long polling.")
-        updater.start_polling(timeout=15, read_latency=4)
+        updater.start_polling(poll_interval = 1.0,timeout=20)
 
     updater.idle()
 
